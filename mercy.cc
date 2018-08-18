8295876b394e7bd3443b70b5950f23f78a6d4751
sohnryang <loop.infinitely@gmail.com>
2018-08-18 23:52:24 +0900

    solved mercy

---
+++ mercy.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+
+int main() {
+    int count;
+    cin >> count;
+    for (int i = 0; i < count; ++i) cout << "Hello Algospot!\n";
+    return 0;
+}
